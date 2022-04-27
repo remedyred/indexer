ecd5805d484e8bb535263c8f551c60d5665e983d
snickbit <nicklowe@snickbit.com>
2022-04-26 20:41:45 -0400

    wip: work

--- packages/releaser/src/actions/release.ts
+++ packages/releaser/src/actions/release.ts
@@ -1,16 +1,24 @@
 import {cli} from '@snickbit/node-cli'
 import {out} from '@snickbit/out'
-import {ask, confirm, fileExists, getFileJson} from '@snickbit/node-utilities'
+import {ask, confirm, fileExists, getFile, getFileJson, saveFile, saveFileJson} from '@snickbit/node-utilities'
 import fg from 'fast-glob'
 import os from 'os'
 import semverInc from 'semver/functions/inc'
-import {execaCommand} from 'execa'
+import {execa, execaCommand} from 'execa'
+import {interpolate} from '@snickbit/utilities'
+import changelog from 'changelog-io'
+import * as path from 'path'
 
-const releaseOptions = {
-	git: {
-		commitMessage: 'chore(release): publish',
-		tagName: '{{name}}@${version}'
-	}
+interface PackageJson {
+	name: string
+	version: string
+	scripts?: Record<string, string>
+}
+
+export interface ReleaserConfig {
+	workspaces: string[]
+	commitMessage: string
+	tagName: string
 }
 
 interface Argv {
@@ -20,12 +28,22 @@ interface Argv {
 }
 
 interface ReleaseConfig {
-	name?: string
-	bump?: 'patch' | 'minor' | 'major' | number | boolean
-	packageDir?: string
-	packagePath?: string,
-	script?: string
+	name: string
+	bump: 'patch' | 'minor' | 'major' | number | boolean
+	packageDir: string
+	packagePath: string
+	pkg: PackageJson
+	version: string
+	dryRun: boolean
 }
+
+const defaultConfig: ReleaserConfig = {
+	workspaces: [],
+	commitMessage: 'chore(release): publish',
+	tagName: '${name}@${version}'
+}
+
+let releaser: ReleaserConfig
 
 export default async args => cli(args)
 .arg('bump')
@@ -41,7 +59,7 @@ export default async args => cli(args)
 	out.fatal('Ready to release', argv)
 
 	const workspaces = await getWorkspaces(argv)
-	let configs: Partial<ReleaseConfig>[] = []
+	let configs: ReleaseConfig[] = []
 	let applyToAll: boolean
 	if (argv.bump) applyToAll = true
 
@@ -58,29 +76,27 @@ export default async args => cli(args)
 				out.warn(`Skipping private package: {magenta}${pkg.name}{/magenta}`)
 				continue
 			}
-			if (!pkg?.scripts?.release) {
-				out.warn(`Skipping package {magenta}${pkg.name}{/magenta} without release script`)
-				continue
-			}
-			const script = pkg.scripts.release
 			const {name, version} = pkg
-			const patch = semverInc(version, 'patch')
-			const minor = semverInc(version, 'minor')
-			const major = semverInc(version, 'major')
+
+			const versions = {
+				patch: semverInc(version, 'patch'),
+				minor: semverInc(version, 'minor'),
+				major: semverInc(version, 'major')
+			}
 
 			const bump = argv.bump || applyToAll || await ask(`Bump version for ${name} (${version})?`, {
 				type: 'select',
 				choices: [
 					{
-						title: `Patch (${patch})`,
+						title: `Patch (${versions.patch})`,
 						value: 'patch'
 					},
 					{
-						title: `Minor (${minor})`,
+						title: `Minor (${versions.minor})`,
 						value: 'minor'
 					},
 					{
-						title: `Major (${major})`,
+						title: `Major (${versions.major})`,
 						value: 'major'
 					},
 					{
@@ -105,8 +121,10 @@ export default async args => cli(args)
 				name,
 				packageDir,
 				packagePath,
+				pkg,
 				bump,
-				script
+				dryRun: argv.dryRun,
+				version: versions[bump]
 			})
 		}
 	}
@@ -117,34 +135,102 @@ export default async args => cli(args)
 
 	for (let config of configs) {
 		if (processes.length >= maxProcesses) await Promise.all(processes)
-		if (argv.dryRun) {
-			out.force.warn(`Dry run: {magenta}${config.name}{/magenta}`, `{cyan}${config.script}{/cyan}`)
-		} else {
-			processes.push(execaCommand(`${config.script} ${config.bump}`, {cwd: config.packageDir, stdio: 'inherit'}))
-		}
+		processes.push(bumpPackage(config))
 	}
 	await Promise.all(processes)
 	out.block.done('Done')
 })
 .catch(err => out.error(err))
 
+async function getConfig(argv) {
+	if (releaser) return releaser
 
-async function getWorkspaces(argv): Promise<string[]> {
-	if (fileExists('releaser.config.json')) {
-		const config = getFileJson('releaser.config.json')
-		if (config.workspaces) {
-			return config.workspaces
+	let config: any = {}
+	if (argv.config && fileExists(argv.config)) {
+		config = getFileJson(argv.config)
+	}
+
+	if (!config.workspaces && fileExists('releaser.config.json')) {
+		config = getFileJson('releaser.config.json')
+	}
+
+	if (!config.workspaces && fileExists('package.json')) {
+		const pkg = getFileJson('package.json')
+		if (pkg.releaser) {
+			config = pkg.releaser
+		} else if (pkg.workspaces) {
+			config = {workspaces: pkg.workspaces}
 		}
 	}
 
-	if (fileExists('package.json')) {
-		const config = getFileJson('package.json')
-		if (config.workspaces) {
-			return config.workspaces
-		} else if (config.releaser.workspaces) {
-			return config.releaser.workspaces
-		}
+	if (config.workspaces) {
+		releaser = Object.assign(defaultConfig, config) as ReleaserConfig
+		return releaser
 	}
 
 	out.fatal('No workspaces defined')
+}
+
+async function getWorkspaces(argv): Promise<string[]> {
+	const config = await getConfig(argv)
+	return config.workspaces
+}
+
+async function bumpPackage(config: ReleaseConfig) {
+	const {name, packageDir, packagePath, bump, version, pkg} = config
+	const _out = out.clone(pkg.name)
+
+	const {scripts} = pkg
+
+	if (scripts?.prerelease) {
+		_out.info(`Running prerelease script`)
+		await execaCommand(scripts.prerelease, {cwd: packageDir})
+	}
+
+	_out.info(`Bumping version to {magenta}${version}{/magenta}`)
+	pkg.version = version
+	saveFileJson(packagePath, pkg)
+
+	const commitMessage = interpolate(releaser.commitMessage, {
+		name,
+		version,
+		bump
+	})
+
+	const tagName = interpolate(releaser.tagName, {
+		name,
+		version,
+		bump
+	})
+
+	_out.info('Generating change log')
+	const changeFilePath = path.join(packageDir, 'CHANGELOG.md')
+	let changeFile = getFile(changeFilePath, '')
+	changeFile = '# Changelog\n\n' + changelog(tagName) + '\n\n' + changeFile.replace(/# Changelog\s+/, '').trim()
+	saveFile(changeFilePath, changeFile)
+
+	_out.info('Committing changes')
+	await execa('git', ['add', config.packagePath, changeFilePath])
+	await execa('git', ['commit', '-m', `'${commitMessage}'`])
+
+	_out.info('Tagging release')
+	await execa('git', ['tag', '-a', '-m', `'${tagName}'`])
+
+	_out.info('Pushing changes')
+	await execa('git', ['push', 'origin', '--tags'])
+
+	if (scripts?.prepublish) {
+		_out.info(`Running prepublish script`)
+		await execaCommand(scripts.prepublish, {cwd: packageDir})
+	}
+
+	_out.info('Publishing package')
+	await execa('npm', ['publish'], {cwd: packageDir})
+
+	if (scripts?.postpublish) {
+		_out.info(`Running postpublish script`)
+		await execaCommand(scripts.postpublish, {cwd: packageDir})
+	}
+
+	_out.block.success('Done')
 }
