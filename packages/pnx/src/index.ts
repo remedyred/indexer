a1a45ae99cc2c237804bdb484a1d9d65127cfaa8
snickbit <nicklowe@snickbit.com>
2022-05-19 22:35:48 -0400

    init: pnx

---
+++ packages/pnx/src/index.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+import {out} from '@snickbit/out'
+import cli from '@snickbit/node-cli'
+import packageJson from '../package.json'
+
+cli()
+.name('@remedyred/pnx')
+.version(packageJson.version)
+.run().then(async (/* argv */) => {
+	out.info('Hello, world!')
+	out.done('Done!')
+})
+.catch(err => out.fatal('Error:', err))
