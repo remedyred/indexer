2fb6a704eccb49346f261450b49be366d63dd6e6
Snickbit <snickbit@users.noreply.github.com>
2022-11-15 15:28:59 -0500

    refactor: split main cli into multiple functions & add state

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import {ask, confirm, fileExists, getFileJson, saveFileJson} from '@snickbit/node-utilities'
 import {lilconfig} from 'lilconfig'
-import {$out, DEFAULT_CONFIG_NAME} from './common'
+import {$out, $state, DEFAULT_CONFIG_NAME, useState} from './common'
 import {AppConfig, IndexerConfig} from './definitions'
 import {objectExcept} from '@snickbit/utilities'
 import {generateIndexes} from './'
@@ -28,52 +28,65 @@ cli().name(packageJson.name)
 			describe: 'Dry run, do not write to disk'
 		}
 	})
-	.run(async argv => {
-		const config: AppConfig = {
+	.run(main)
+
+async function main(argv) {
+	const config = await useConfig(argv)
+	const {configPath} = useState() // must come after first useConfig
+
+	await generate()
+
+	if (!config.dryRun && !configPath && await confirm('Do you want to save the configuration?')) {
+		const save_path = configPath || await ask('Path to save config file?', DEFAULT_CONFIG_NAME)
+		if (!save_path) {
+			$out.fatal('No path provided')
+		}
+		await saveFileJson(save_path, $state.config.indexer)
+	}
+
+	$out.done('Done')
+}
+
+async function useConfig(argv?: any): Promise<AppConfig> {
+	if (!$state.config) {
+		$state.config = {
 			source: argv.source,
 			output: argv.output,
 			dryRun: argv.dryRun
 		}
 
-		if (config.source && !config.source.includes('*')) {
-			config.source = `${config.source}/**/*`
+		if ($state.config.source && !$state.config.source.includes('*')) {
+			$state.config.source = `${$state.config.source}/**/*`
 		}
 
-		let configPath
-
 		if (argv.config && argv.config !== 'false' && fileExists(argv.config)) {
-			configPath = argv.config
-			config.indexer = getFileJson(argv.config)
+			$state.configPath = argv.config
+			$state.config.indexer = getFileJson(argv.config)
 		} else {
 			const result = await lilconfig('indexer').search()
 			if (result) {
-				configPath = result.filepath
-				config.indexer = result.config
+				$state.configPath = result.filepath
+				$state.config.indexer = result.config
 			}
 		}
+	}
+	return $state.config
+}
 
-		if (config.indexer || config.source) {
-			const conf = config.indexer as IndexerConfig
-			if (conf?.indexes) {
-				const root: Omit<IndexerConfig, 'indexes'> = objectExcept(conf, ['indexes']) as IndexerConfig
-				for (const key in conf.indexes) {
-					conf.indexes[key] = await generateIndexes(config, {...root, ...conf.indexes[key]}) as IndexerConfig
-				}
-				config.indexer = conf
-			} else {
-				config.indexer = await generateIndexes(config)
+async function generate() {
+	const {config} = useState()
+	if (config.indexer || config.source) {
+		const conf = config.indexer as IndexerConfig
+		if (conf?.indexes) {
+			const root: Omit<IndexerConfig, 'indexes'> = objectExcept(conf, ['indexes']) as IndexerConfig
+			for (const key in conf.indexes) {
+				conf.indexes[key] = await generateIndexes(config, {...root, ...conf.indexes[key]}) as IndexerConfig
 			}
+			config.indexer = conf
 		} else {
-			$out.fatal('No configuration found and no source directory specified')
+			config.indexer = await generateIndexes(config)
 		}
-
-		if (!config.dryRun && !configPath && await confirm('Do you want to save the configuration?')) {
-			const save_path = configPath || await ask('Path to save config file?', DEFAULT_CONFIG_NAME)
-			if (!save_path) {
-				$out.fatal('No path provided')
-			}
-			await saveFileJson(save_path, config.indexer)
-		}
-
-		$out.done('Done')
-	})
+	} else {
+		$out.fatal('No configuration found and no source directory specified')
+	}
+}
