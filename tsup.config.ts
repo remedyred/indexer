e2268548a1d243000238d09acb47957c4d1d3fb9
Snickbit <snickbit@users.noreply.github.com>
2023-07-19 21:43:24 -0400

    feat: add esbuild/tsup plugin

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,11 @@ import {defineConfig} from 'tsup'
 
 export default defineConfig({
 	// bundle: false,
-	entry: ['src/index.ts', 'src/cli.ts'],
+	entry: [
+		'src/index.ts',
+		'src/cli.ts',
+		'src/esbuild.ts'
+	],
 	clean: true,
 	splitting: false,
 	format: ['esm'],
