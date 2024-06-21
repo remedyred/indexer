fd490ecf57c0b41d2ab89b4dee0017078aa806ee
Snickbit <snickbit@users.noreply.github.com>
2024-06-21 17:04:20 -0400

    refactor(plugin): change indexerPlugin to factory function
    - Changed the indexerPlugin into a factory function for plugins
    - This refactoring involves no changes to functionality or outputs, just to the organization of the code.

--- src/plugin.ts
+++ src/plugin.ts
@@ -4,21 +4,26 @@ import {$out} from '@/common'
 import fs from 'fs'
 import indexer from '@/index'
 
-export default function indexerPlugin(): Plugin {
-	return {
-		name: 'indexer',
-		setup(build: PluginBuild) {
-			$out.prefix('IDX')
-			build.onStart(async (): Promise<void> => {
-				const {path: indexerPath} = await build.resolve('indexer.config.json', {kind: 'require-resolve', resolveDir: process.cwd()})
-				if (indexerPath && fs.existsSync(indexerPath)) {
-					const indexerConfig = getFileJSON(indexerPath)
-					// Pass the config object to the indexer
-					await indexer(indexerConfig)
-				} else {
-					$out.warn(`indexer.config.json not found at ${indexerPath}`)
-				}
+const plugin: Plugin = {
+	name: 'indexer',
+	setup(build: PluginBuild) {
+		$out.prefix('IDX')
+		build.onStart(async (): Promise<void> => {
+			const {path: indexerPath} = await build.resolve('indexer.config.json', {
+				kind: 'require-resolve',
+				resolveDir: process.cwd()
 			})
-		}
+			if (indexerPath && fs.existsSync(indexerPath)) {
+				const indexerConfig = getFileJSON(indexerPath)
+				// Pass the config object to the indexer
+				await indexer(indexerConfig)
+			} else {
+				$out.warn(`indexer.config.json not found at ${indexerPath}`)
+			}
+		})
 	}
+}
+
+export default function indexerPluginFactory(): Plugin {
+	return plugin
 }
